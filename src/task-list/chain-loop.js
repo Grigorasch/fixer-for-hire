b44a3a54dcd003c7ddc0ec0125c78e5de1b6c39c
grigorasch <pgv856@gmail.com>
2023-05-30 21:01:12 +0600

    Реализован конструктор и базовые методы класса ChainLoop

--- src/task-list/chain-loop.js
+++ src/task-list/chain-loop.js
@@ -1,5 +1,37 @@
 export default class ChainLoop {
-    constructor() {
-        
+    constructor(prevChain = null) {
+      if (prevChain.toString() === 'ChainLoop') {
+        this.start = prevChain.start;
+        this.end = prevChain.end;
+        this.prev = prevChain;
+        this.next = prevChain.next;
+        this.cur = null;
+        this.enum = () => this.prev.enum() + 1;
+        this.prev.setNextChain(this);
+        this.next.setPrevChain(this);
+      } else {
+        this.start = this;
+        this.end = null;
+        this.prev = null;
+        this.next = null;
+        this.cur = null;
+        this.enum = () => 0;
+      }
+    }
+    
+    setPrevChain(newPrevChain) {
+      this.prev = newPrevChain;
+    }
+    
+    setNextChain(newNextChain) {
+      this.next = newNextChain;
+    }
+    
+    setContent(content) {
+      this.cur = content
+    }
+    
+    toString() {
+      return 'ChainLoop';
     }
 }
