4a21e9b37b4d19e6b5c085baee7d8789c7120ff2
Grigorasch <pgv856@gmail.com>
2023-05-30 06:34:45 +0600

    Создана основа тестов для класса ChainLoop

---
+++ test/task-list/chain-loop.test.js
@@ -0,0 +1,8 @@
+import { assert } from "chai";
+import { describe } from "mocha";
+
+describe('Класс ChainLoop для организации связанного списка', function () {
+    describe('Конструктор класса', function () {
+
+    });
+});
