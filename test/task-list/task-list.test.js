adc3253f3435e4f6dc42c4d8cceb5f31f604358d
Grigorasch <pgv856@gmail.com>
2023-05-31 07:38:25 +0600

    Создан базовый шаблон теста

---
+++ test/task-list/task-list.test.js
@@ -0,0 +1,8 @@
+import { assert } from "chai";
+import { describe } from "mocha";
+
+describe('Класс TaskList реализующий связаный список для очереди задач, ожидающие выполнение, а также интерфейс для управления списком', function () {
+    describe('Конструктор класса', function () {
+        
+    });
+});
