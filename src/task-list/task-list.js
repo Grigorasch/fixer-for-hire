554f972087a955be27d124c9e10458154c815378
Grigorasch <pgv856@gmail.com>
2023-05-31 07:49:20 +0600

    Исправлена реализация TaskLoop. Настроены значения полей по умолчанию

--- src/task-list/task-list.js
+++ src/task-list/task-list.js
@@ -2,47 +2,35 @@ export default class TaskList {
 
 }
 
-/**
- * Функция конструктор объекта, который является узловым элементом связанного списка
- *
- * @param {{ taskFunction: any; taskPriority?: number; }} { taskFunction, taskPriority = 0 }
- * @param {{}} [nodeOption={}]
- * @returns
- */
 function TaskLoop(taskFunction,
     {
         taskOption: {
             priority = 0,
             callback = null,
-        },
+        } = {},
         nodeOption: {
             start = null,
             end = null,
             prev = null,
             next = null,
-        }
-    }) { // task = { taskFunction, taskPriority: 0 }, nodeOption = {}) {
-    // const task = {
-    //     taskFunction,
-    //     taskPriority
-    // }
+        } = {}
+    } = {}) {
     this.task = {
         get taskSolver() {
             return () => taskFunction();
         },
-        taskImportance: task.taskPriority
+        taskImportance: priority
     };
 
-    const NULL_NODE_OPTION = {
-        start: null,
-        end: null,
-        prev: null,
-        next: null
-    }
-    this.list = Object.assign({}, NULL_NODE_OPTION, nodeOption);
+    this.list = {
+        start: start,
+        end: end,
+        prev: prev,
+        next: next
+    };
 }
 
-const tOpt = { priority: 1 };
-const nOpt = { start: {}, end: {} }
-let a = new TaskLoop(()=> {}, tOpt, nOpt);
+const taskOption = { priority: 1 };
+const nodeOption = { start: {}, end: {} }
+let a = new TaskLoop(() => { }, { taskOption, nodeOption });
 console.log(a);
